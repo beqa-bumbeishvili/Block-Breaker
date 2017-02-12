0ee1d97beddba780abd13620cde346a7ef2c975f
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-02-12 12:16:44 +0400

    initial commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+	
+    public void LoadLevelOneScene()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void LoadWinScene()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
